9c64fc49ba78ee5c24dcb91acb02c35639af4997
Rayable <rayable11.0808@gmail.com>
2026-03-23 21:44:55 +0800

    Added basic app infrastructure

---
+++ localAgent/src/client/Home.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './Home.css'
+
+function Home() {
+  const [count, setCount] = useState(0);
+  return(<h1>hello</h1>)
+}
+ export default Home
