845f165495d5f50904cf65338c304e2eb3db8ae0
Rayable <rayable11.0808@gmail.com>
2026-03-24 22:06:25 +0800

    basic UI additions

--- localAgent/src/client/Home.jsx
+++ localAgent/src/client/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/navbar'
 import Bubble from './components/bubble'
 import './Main.css'
 
 function Home() {
   const [count, setCount] = useState(0);
-    return(<div>
-    <Bubble/>
-    </div>)
+    return(
+    <Navbar/>
+    )
 }
  export default Home
