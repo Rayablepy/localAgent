6fac3ec0ead2e147fa263ac004b2d169943380d4
Rayable <rayable11.0808@gmail.com>
2026-03-24 19:16:46 +0800

    added some styling, included tailwind, updated bubble and css accordingly

--- localAgent/src/client/Home.jsx
+++ localAgent/src/client/Home.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './Home.css'
+import Bubble from './components/bubble'
+import './Main.css'
 
 function Home() {
   const [count, setCount] = useState(0);
-  return(<h1>hello</h1>)
+    return(<div>
+    <Bubble/>
+    </div>)
 }
  export default Home
--- localAgent/src/client/components/bubble.jsx
+++ localAgent/src/client/components/bubble.jsx
@@ -1,5 +1,15 @@
+import './components.css';
+
 export default function Bubble() {
-  return(<div>
-    <div><p></p></div>
-  </div>)
+  return (
+      <div className="flex items-start gap-2.5">
+        <div
+            className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 bg-neutral-secondary-soft rounded-e-base rounded-es-base">
+          <div className="flex items-center space-x-1.5 rtl:space-x-reverse">
+            <span className="text-amber-50 text-sm font-semibold text-heading">User</span>
+          </div>
+          <p className="text-amber-50 text-sm py-2.5 text-body">Some text</p>
+        </div>
+      </div>
+  )
 }
