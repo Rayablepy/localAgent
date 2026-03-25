0e8884b36743d8e20cda89034d594d74ba98c2cd
Rayable <rayable11.0808@gmail.com>
2026-03-25 19:07:34 +0800

    added basic styling and chat ui,with a bit of usestate logic

--- localAgent/src/client/Home.jsx
+++ localAgent/src/client/Home.jsx
@@ -1,11 +1,44 @@
-import { useState } from 'react'
-import Navbar from './components/navbar'
-import Bubble from './components/bubble'
-import './Main.css'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import UserBubble from "./components/UserBubble";
+import IncomingBubble from "./components/IncomingBubble";
+import ChatInput from "./components/ChatInput";
+import "./components/components.css";
+import "./home.css"
 
-function Home() {
-    return( 
-    <div></div>
-    )
+export default function Home() {
+    //placeholder
+  const [messages, setMessages] = useState([
+    { type: "incoming", text: "Hello" },
+    { type: "user", text: "Hi" }
+  ]);
+
+  const handleSend = (msg) => {
+    setMessages((prev) => [...prev, { type: "user", text: msg }]);
+
+    //fake reply
+    setTimeout(() => {
+      setMessages((prev) => [
+        ...prev,
+        { type: "incoming", text: "some texxt" }
+      ]);
+    }, 500);
+  };
+
+  return (
+    <div className="chat-root h-screen flex flex-col">
+      <Navbar />
+
+      <div className="flex-1 overflow-y-auto px-3 py-4 space-y-3">
+        {messages.map((msg, i) =>
+          msg.type === "user" ? (
+            <UserBubble key={i} message={msg.text} />
+          ) : (
+            <IncomingBubble key={i} message={msg.text} />
+          )
+        )}
+      </div>
+      <ChatInput onSend={handleSend} />
+    </div>
+  );
 }
- export default Home
