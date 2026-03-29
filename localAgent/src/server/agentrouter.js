b1a8be9e32b2ca229edc4b1f5abaf47f3ab0751d
Rayable <rayable11.0808@gmail.com>
2026-03-29 20:25:17 +0800

    added test tools(does not work yet)

--- localAgent/src/server/agentrouter.js
+++ localAgent/src/server/agentrouter.js
@@ -1,18 +1,32 @@
+import { tool } from "@langchain/core/tools";
+import { ChatOllama } from "@langchain/ollama";
+import * as z from "zod";
 
-import { ChatOllama } from "@langchain/ollama"
+const test_tool = tool(
+    async ({ name })=> {
+        return `test_tool describes name as ${name}`;
+    },
+    {
+        name:"test",
+        description: "Test description",
+        schema: z.object({
+            test:  z.string().describe("return some text prepended to the name variable passed in")
+        }),
+    });
 
 const agent = new ChatOllama({
-    model:"llama3",
+    model:"llama3.1:8b",
     temperature: 0,
     maxRetries:2,
 })
+const agentwithtool = agent.bindTools([test_tool]);
+
 export async function getOllamaResponse(message) {
-    const response = await agent.invoke([
+    const response = await agentwithtool.invoke([
         ["system", "You are a local AI agent running on user machines. You will assist them with any needs and wants using the tools that will be at your disposal(tools not yet implemented)"],
         ["user",message]
     ]);
-    //console.log(response)
-    return response.content;
+    return response.tool_calls;
 }
 
 
