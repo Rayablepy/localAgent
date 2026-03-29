9d05d9e4c03a9e6db9fc0f3e796466fb0b17e546
Rayable <rayable11.0808@gmail.com>
2026-03-29 20:06:51 +0800

    langchain integration

--- localAgent/src/server/agentrouter.js
+++ localAgent/src/server/agentrouter.js
@@ -1,15 +1,18 @@
-import ollama from 'ollama';
 
+import { ChatOllama } from "@langchain/ollama"
+
+const agent = new ChatOllama({
+    model:"llama3",
+    temperature: 0,
+    maxRetries:2,
+})
 export async function getOllamaResponse(message) {
-    const response = await ollama.chat({
-        model: 'llama3',
-        messages: [
-            {role: "system", content: "You are a local AI agent running on user machines. You will assist them with any needs and wants using the tools that will be at your disposal(tools not yet implemented)"},
-            { role: "user", content: message }
-        ]
-    });
-
-    return response.message.content;
+    const response = await agent.invoke([
+        ["system", "You are a local AI agent running on user machines. You will assist them with any needs and wants using the tools that will be at your disposal(tools not yet implemented)"],
+        ["user",message]
+    ]);
+    //console.log(response)
+    return response.content;
 }
 
 
