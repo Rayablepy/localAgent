8cb5eb741571ef117bd8adc0cf6c1ca3c3193aa8
Rayable <rayable11.0808@gmail.com>
2026-04-07 13:11:35 +0800

    added MORE changes

--- localAgent/src/server/chatmemory/chatmemoryservice.js
+++ localAgent/src/server/chatmemory/chatmemoryservice.js
@@ -1,13 +1,10 @@
 import { obtainData,insertData } from '../db/databasehandler.js';
-import { messagelog } from '/localAgent/src/client/Home.jsx'
+import messagelog from '/localAgent/src/client/Home.jsx'
 
-const usercontent = messagelog.content[0];
-const aicontent = messagelog.content[1];
-
-const insertionresponse = await insertData('INSERT INTO messages (role,content) VALUES ("user","some text")');
+const insertionresponse = await insertData("INSERT INTO messages (role,content) VALUES ('user','some text')");
 console.log(insertionresponse);
-
-const dataresponse = await obtainData('SELECT * FROM messages');
+console.log(messagelog.content[0])
+const dataresponse = await obtainData("SELECT * FROM messages");
 console.log(dataresponse);
 
 
