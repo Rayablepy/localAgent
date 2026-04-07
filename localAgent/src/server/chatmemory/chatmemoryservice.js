55a81b2b28d4a74630f1ee0849947af37f525f93
Rayablepy <rayhan11.0808@gmail.com>
2026-04-07 10:14:02 +0800

    update chatmemoryservice.js with messagelog import and untested usage

--- localAgent/src/server/chatmemory/chatmemoryservice.js
+++ localAgent/src/server/chatmemory/chatmemoryservice.js
@@ -1,6 +1,10 @@
 import { obtainData,insertData } from '../db/databasehandler.js';
+import { messagelog } from '/localAgent/src/client/Home.jsx'
 
-const insertionresponse = await insertData('INSERT INTO messages (role,content) VALUES ("user","Some text")');
+const usercontent = messagelog.content[0];
+const aicontent = messagelog.content[1];
+
+const insertionresponse = await insertData('INSERT INTO messages (role,content) VALUES ("user","some text")');
 console.log(insertionresponse);
 
 const dataresponse = await obtainData('SELECT * FROM messages');
