6d607e832ef4db548e0a7b1b88c791e28d0f7145
Rayable <rayable11.0808@gmail.com>
2026-06-19 12:09:09 +0100

    added basic python backbone

---
+++ server/main.py
@@ -0,0 +1,14 @@
+from langchain.agents import create_agent
+model = create_agent(
+    model="ollama:qwen3.5:0.8b",
+    system_prompt="You are my helpful assistant"
+)
+
+while True:
+    user = str(input("Prompt or q to quit: "))
+    if user.lower() == "q":
+        break
+    result = model.invoke(
+        {"messages": [{"role": "user", "content": user}]}
+    )
+    print(result["messages"][-1].content_blocks)
