0ca845b008c297e5fd582b4e64195440b3ec35ad
Rayable <rayable11.0808@gmail.com>
2026-07-05 23:00:14 +0800

    formatted CLI based conversation and removed backend for now

--- agent.py
+++ agent.py
@@ -1,11 +1,35 @@
+import asyncio
 from langchain.chat_models import init_chat_model
 from deepagents import create_deep_agent
 
-model = create_deep_agent(init_chat_model(
+model = init_chat_model(
         model="running-model",
         model_provider="openai",
         base_url="http://localhost:1234/v1",
         api_key="not-needed",
-        temperature=0.6
-    ))
+        temperature=0.5
+)
+system="""
+You are the user's personal AI assistant running locally on their computer. Your tools are not yet created, but you will 
+be able to accept and read documents and files that the user gives you
+"""
+
+
+agent = create_deep_agent(model=model,system_prompt=system)
+
+async def response(message:str):
+    return await agent.ainvoke(
+        {"messages": [{"role": "user", "content": message}]},
+    )
+
+while True:
+    user = str(input("Enter prompt: ")).lower()
+    if user == "q":
+        break
+    print("-"*100)
+    modelresponse = asyncio.run(response(user))
+    print(modelresponse["messages"][-1].content_blocks[0]["text"])
+    print("-"*100)
+
+
 
