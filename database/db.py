c55d336bda63a496842bdfe2357639bd82d1b5b9
Rayable <rayable11.0808@gmail.com>
2026-07-28 22:59:30 +0800

    added todo list table and initialised db

--- database/db.py
+++ database/db.py
@@ -1,8 +1,18 @@
 import sqlite3
 from pathlib import Path
+
 SCRIPT_DIR = Path(__file__).resolve().parent
 DB_PATH = SCRIPT_DIR / "database.db"
 
 conn = sqlite3.connect(DB_PATH)
 
 cursor=conn.cursor()
+cursor.execute(
+    "CREATE TABLE IF NOT EXISTS TodoList ("
+    "id INTEGER PRIMARY KEY, name TEXT, information TEXT, timestamp TEXT,complete BOOLEAN)"
+)
+try:
+    conn.commit()
+    print("Table created successfully")
+except sqlite3.OperationalError:
+    print("Error executing table creation")
