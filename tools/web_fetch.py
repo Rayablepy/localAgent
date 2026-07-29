db5138f80e480b0b1aff68460b8d0e1fd26f53eb
Rayablepy <rayhan11.0808@gmail.com>
2026-07-30 07:17:12 +0800

    added markdown integration for optimal agent ingestion

--- tools/web_fetch.py
+++ tools/web_fetch.py
@@ -1,4 +1,5 @@
 from langchain_core.tools import tool
+from markdownify import markdownify as md
 import httpx
 import asyncio
 
@@ -6,5 +7,6 @@ async def web_fetch():
     async with httpx.AsyncClient() as client:
         response = await client.get("https://books.toscrape.com")
         response = response.text
+        print(md(response,strip=['script']))
 
 asyncio.run(web_fetch())
