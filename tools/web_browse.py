55ae14e192cd6ad7b231029c79c4ce6d964b7ec8
Rayablepy <rayhan11.0808@gmail.com>
2026-07-30 19:59:18 +0800

    added playwright web search for links

--- tools/web_browse.py
+++ tools/web_browse.py
@@ -1,10 +1,10 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.tools import tool
 from langchain.agents import create_agent
-from config.settings import SUBAGENT_MODEL_NAME
 
+from playwright.sync_api import sync_playwright
 subagent_model = init_chat_model(
-        model=SUBAGENT_MODEL_NAME,
+        model="",
         model_provider="openai",
         base_url="http://localhost:1234/v1",
         api_key="not-needed",
@@ -12,7 +12,20 @@ subagent_model = init_chat_model(
 )
 
 subagent = create_agent(model=subagent_model)
-@tool
-def web_browse(instructions: str)->str:
-    result=subagent.invoke({"messages": [{"role": "user", "content": instructions}]})
-    return result
+
+def web_browse(search: str)->str:
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+        page.wait_for_load_state("domcontentloaded")
+        search_box = page.locator("textarea[name='q']")
+        search_box.fill(search)
+        search_box.press("Enter")
+        page.wait_for_load_state("domcontentloaded")
+        page.wait_for_selector("h3")
+        links = page.locator("h3").all()
+        return(f"All links found related to search query : \n{links}")
+        browser.close()
+
+print(web_browse("Nanyang polytechnic"))
